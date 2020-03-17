67c46400db9cec6d78f6e578d8082030c1bc07a2
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-03-17 15:28:12 -0700

    Completed MVP by created react components and passing data via props to create the instgram page. Currently attempting stretch goals

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,14 +3,18 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+// import dummyData from "../../dummy-data";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map( data => (
+        <Comment comment={data} />
+        ))}
     </div>
   );
 };
